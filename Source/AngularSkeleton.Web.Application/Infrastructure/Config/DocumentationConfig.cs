78c8f4b8ac43726e5a7fc1b2910c8a36235f1c4a
Christopher Town <christopher@christophertown.com>
2015-10-21 20:46:17 -0500

    Added swashbuckle models

--- Source/AngularSkeleton.Web.Application/Infrastructure/Config/DocumentationConfig.cs
+++ Source/AngularSkeleton.Web.Application/Infrastructure/Config/DocumentationConfig.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Reflection;
 using System.Web.Http;
 using Swashbuckle.Application;
 
@@ -20,22 +19,22 @@ namespace AngularSkeleton.Web.Application.Infrastructure.Config
             config
                 .EnableSwagger(c =>
                 {
+                    // core assembly comments
                     var baseDirectory = AppDomain.CurrentDomain.BaseDirectory + "\\Bin";
-                    var commentsFileName = Assembly.GetExecutingAssembly().GetName().Name + ".xml";
-                    var commentsFile = Path.Combine(baseDirectory, commentsFileName);
+                    c.IncludeXmlComments(Path.Combine(baseDirectory, "AngularSkeleton.Web.Application.xml"));
+                    c.IncludeXmlComments(Path.Combine(baseDirectory, "AngularSkeleton.Service.Model.xml"));
 
-                    c.SingleApiVersion("v1", "Envoice API")
-                        .Description("A sample API for testing and prototyping Swashbuckle features")
+                    // models assembly comments
+
+                    c.SingleApiVersion("v1", "AngularSkeleton.NET")
+                        .Description("Angular skeleton application API")
                         .TermsOfService("Some terms")
                         .Contact(cc => cc
-                            .Name("Your name")
-                            .Url("http://www.angularskeleton.com/contact")
-                            .Email("info@angularskeleton.com"))
+                            .Name("Christopher Town")
+                            .Url("https://github.com/christophla/AngularSkeleton.NET"))
                         .License(lc => lc
                             .Name("License")
-                            .Url("http://www.angularskeleton.com/license"));
-
-                    c.IncludeXmlComments(commentsFile);
+                            .Url("https://github.com/christophla/AngularSkeleton.NET/blob/master/LICENSE"));
 
                     c
                         .OAuth2("oauth2")
